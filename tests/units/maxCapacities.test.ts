9c25a1adb966531abad2e7e2f575361fcd9198c3
mlgarchery <martial.garchery@gmail.com>
2023-05-23 17:02:13 +0200

    fix: add repaying over supply cap test

--- tests/units/maxCapacities.test.ts
+++ tests/units/maxCapacities.test.ts
@@ -15,15 +15,19 @@ describe("getUserMaxCapacity", () => {
 
   beforeEach(async () => {
     adapter = MorphoAaveV3Adapter.fromMock(ADAPTER_MOCK);
-    await adapter.refreshAll();
     await adapter.connect(userAddress);
+    await adapter.refreshAll();
   });
 
   describe("should be limited by the wallet balance", () => {
-    const expectedMaxBalance = ADAPTER_MOCK.userMarketsData[Underlying.dai].walletBalance;
+    const expectedMaxBalance =
+      ADAPTER_MOCK.userMarketsData[Underlying.dai].walletBalance;
 
     it("when supplying", async () => {
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.supply);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.supply
+      );
       expect(maxCapacity?.amount).toBnEq(expectedMaxBalance);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.walletBalance);
     });
@@ -34,17 +38,26 @@ describe("getUserMaxCapacity", () => {
         TransactionType.supplyCollateral
       );
       expect(maxCapacityCollateral?.amount).toBnEq(expectedMaxBalance);
-      expect(maxCapacityCollateral?.limiter).toBe(MaxCapacityLimiter.walletBalance);
+      expect(maxCapacityCollateral?.limiter).toBe(
+        MaxCapacityLimiter.walletBalance
+      );
     });
 
     it("when repaying", async () => {
-      let maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.repay);
+      let maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.repay
+      );
 
       // in the default mock state the totalBorrow is < walletBalance
       // so the limiter is the totalBorrow
-      const totalBorrow = adapter.getUserMarketsData()[Underlying.dai]!.totalBorrow;
+      const totalBorrow =
+        adapter.getUserMarketsData()[Underlying.dai]!.totalBorrow;
       expect(maxCapacity?.amount).toBnEq(totalBorrow);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.balance);
+      expect(ADAPTER_MOCK.marketsConfigs[Underlying.dai].supplyCap).toBnEq(
+        constants.Zero
+      ); // no cap
 
       const mock = {
         ...ADAPTER_MOCK,
@@ -59,13 +72,65 @@ describe("getUserMaxCapacity", () => {
       };
 
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.repay);
+      maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.repay
+      );
 
       expect(maxCapacity?.amount).toBnEq(totalBorrow.sub(1));
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.walletBalance);
+    });
+
+    it("when repaying over the supplyCap", async () => {
+      const daiSupplyCap =
+        ADAPTER_MOCK.marketsConfigs[Underlying.dai].supplyCap;
+      const daiTotalMorphoSupply =
+        adapter.getMarketsData()[Underlying.dai]!.totalMorphoSupply;
+      const userDaiBorrow =
+        adapter.getUserMarketsData()[Underlying.dai]!.totalBorrow;
+      const isRepayPaused =
+        adapter.getMarketsConfigs()[Underlying.dai]!.isRepayPaused;
+
+      expect(daiSupplyCap).toBnEq(constants.Zero); // no limit on supply, we override this below
+      expect(isRepayPaused).toBe(false);
+
+      const newSupplyCap = daiTotalMorphoSupply.add(userDaiBorrow.div(2));
+
+      const mock = {
+        ...ADAPTER_MOCK,
+        marketsConfigs: {
+          ...ADAPTER_MOCK.marketsConfigs,
+          [Underlying.dai]: {
+            ...ADAPTER_MOCK.marketsConfigs[Underlying.dai],
+            // override the supplyCap to something we can repay over to
+            supplyCap: newSupplyCap,
+          },
+        },
+        userMarketsData: {
+          ...ADAPTER_MOCK.userMarketsData,
+          [Underlying.dai]: {
+            ...ADAPTER_MOCK.userMarketsData[Underlying.dai],
+            // override the wallet balance to the amount we have to repay
+            walletBalance: userDaiBorrow,
+          },
+        },
+      };
+
+      adapter = MorphoAaveV3Adapter.fromMock(mock);
+      await adapter.connect(userAddress);
+      await adapter.refreshAll();
+
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.repay
+      )!;
+
+      expect(maxCapacity.limiter).toBe(MaxCapacityLimiter.walletBalance);
+      // userDaiBorrow == walletBalance
+      expect(maxCapacity.amount).toBnEq(userDaiBorrow); // the other half of the repay balance is going to be idle supply
     });
   });
 
@@ -74,9 +139,16 @@ describe("getUserMaxCapacity", () => {
       const totalBorrow = adapter.getUserData()!.totalBorrow;
       const borrowCapacity = adapter.getUserData()!.borrowCapacity;
 
-      const chainUsdPrice = ADAPTER_MOCK.marketsData[Underlying.usdc].chainUsdPrice;
+      const chainUsdPrice =
+        ADAPTER_MOCK.marketsData[Underlying.usdc].chainUsdPrice;
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.usdc, TransactionType.borrow);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.usdc,
+        TransactionType.borrow
+      );
+      expect(
+        ADAPTER_MOCK.userMarketsData[Underlying.usdc].walletBalance
+      ).toBnGte(maxCapacity?.amount!);
 
       // borrowCapacityLeft
       const expectedMaxCapacity = borrowCapacity
@@ -92,14 +164,20 @@ describe("getUserMaxCapacity", () => {
   });
 
   describe("should be limited by caps", () => {
-    const daiWalletBalance = ADAPTER_MOCK.userMarketsData[Underlying.dai].walletBalance;
+    const daiWalletBalance =
+      ADAPTER_MOCK.userMarketsData[Underlying.dai].walletBalance;
 
-    const scaledPoolSupply = ADAPTER_MOCK.marketsData[Underlying.dai].scaledPoolSupply;
+    const scaledPoolSupply =
+      ADAPTER_MOCK.marketsData[Underlying.dai].scaledPoolSupply;
 
     // timestamp in the mock and global data are equals so the index is not supposed to change, we can consider newPoolSupplyIndex = oldPoolSupplyIndex
-    const newPoolSupplyIndex = ADAPTER_MOCK.marketsData[Underlying.dai].aaveIndexes.liquidityIndex;
+    const newPoolSupplyIndex =
+      ADAPTER_MOCK.marketsData[Underlying.dai].aaveIndexes.liquidityIndex;
 
-    const daiPoolSupply = WadRayMath.rayMul(scaledPoolSupply, newPoolSupplyIndex);
+    const daiPoolSupply = WadRayMath.rayMul(
+      scaledPoolSupply,
+      newPoolSupplyIndex
+    );
 
     it("when supplying ", async () => {
       // We set the supply cap to something above the poolSupply and in the range of the wallet balance. The wallet balance should be > supplyCap - poolSupply so that we hit the cap limiter in this test
@@ -119,10 +197,13 @@ describe("getUserMaxCapacity", () => {
       };
 
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.supply);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.supply
+      );
 
       expect(maxCapacity?.amount).toBnEq(expectedMaxCapacity);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.cap);
@@ -145,8 +226,8 @@ describe("getUserMaxCapacity", () => {
       };
 
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
       const maxCapacity = adapter.getUserMaxCapacity(
         Underlying.dai,
@@ -158,23 +239,28 @@ describe("getUserMaxCapacity", () => {
     });
 
     it("when borrowing", async () => {
-      const scaledPoolBorrow = ADAPTER_MOCK.marketsData[Underlying.usdc].scaledPoolBorrow;
+      const scaledPoolBorrow =
+        ADAPTER_MOCK.marketsData[Underlying.usdc].scaledPoolBorrow;
 
       // we need to take into account the stable borrow (even if morpho doesn't contribute to it)
-      const poolStableBorrow = ADAPTER_MOCK.marketsData[Underlying.usdc].poolStableBorrow;
+      const poolStableBorrow =
+        ADAPTER_MOCK.marketsData[Underlying.usdc].poolStableBorrow;
 
       const newPoolBorrowIndex =
-        ADAPTER_MOCK.marketsData[Underlying.usdc].aaveIndexes.variableBorrowIndex;
+        ADAPTER_MOCK.marketsData[Underlying.usdc].aaveIndexes
+          .variableBorrowIndex;
 
-      const usdcPoolBorrow = WadRayMath.rayMul(scaledPoolBorrow, newPoolBorrowIndex).add(
-        poolStableBorrow
-      );
+      const usdcPoolBorrow = WadRayMath.rayMul(
+        scaledPoolBorrow,
+        newPoolBorrowIndex
+      ).add(poolStableBorrow);
 
       // Snapshoting the borrow capacity
       const totalBorrow = adapter.getUserData()!.totalBorrow;
       const borrowCapacity = adapter.getUserData()!.borrowCapacity;
 
-      const chainUsdPrice = ADAPTER_MOCK.marketsData[Underlying.usdc].chainUsdPrice;
+      const chainUsdPrice =
+        ADAPTER_MOCK.marketsData[Underlying.usdc].chainUsdPrice;
 
       // borrowCapacityLeft
       const borrowCapacityLeft = borrowCapacity
@@ -184,7 +270,9 @@ describe("getUserMaxCapacity", () => {
         .mul(pow10(6))
         .div(chainUsdPrice);
 
-      expect(borrowCapacityLeft.toString()).toMatchInlineSnapshot(`"726206263744"`);
+      expect(borrowCapacityLeft.toString()).toMatchInlineSnapshot(
+        `"726206263744"`
+      );
       //
 
       const expectedMaxCapacity = borrowCapacityLeft.sub(1); // -1 to be sure we are below the borrow capacity, otherwise we would hit the borrow capacity limiter not the borrow cap limiter
@@ -203,17 +291,16 @@ describe("getUserMaxCapacity", () => {
       };
 
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.usdc, TransactionType.borrow);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.usdc,
+        TransactionType.borrow
+      );
 
       expect(maxCapacity?.amount).toBnEq(expectedMaxCapacity);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.cap);
-    });
-
-    it.skip("when repaying", async () => {
-      // TODO tests we hit the supply cap (idle supply is increased)
     });
   });
 
@@ -232,10 +319,13 @@ describe("getUserMaxCapacity", () => {
       };
 
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.withdraw);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.withdraw
+      );
       expect(maxCapacity?.amount).toBe(constants.One);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.poolLiquidity);
     });
@@ -243,25 +333,37 @@ describe("getUserMaxCapacity", () => {
 
   describe("should be limited by user balance", () => {
     it("when withdrawing", async () => {
-      const poolLiquidity = ADAPTER_MOCK.marketsData[Underlying.dai].poolLiquidity;
+      const poolLiquidity =
+        ADAPTER_MOCK.marketsData[Underlying.dai].poolLiquidity;
 
-      const expectedMaxDaiSupplied = adapter.getUserMarketsData()[Underlying.dai]!.totalSupply;
+      const expectedMaxDaiSupplied =
+        adapter.getUserMarketsData()[Underlying.dai]!.totalSupply;
 
-      expect(expectedMaxDaiSupplied.toString()).toMatchInlineSnapshot(`"7094038561468053280148"`);
+      expect(expectedMaxDaiSupplied.toString()).toMatchInlineSnapshot(
+        `"7094038561468053280148"`
+      );
 
       // to be sure we are below the pool liquidity and test the balance limiter
       expect(poolLiquidity).toBnGt(expectedMaxDaiSupplied);
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.withdraw);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.withdraw
+      );
       expect(maxCapacity?.amount).toBnEq(expectedMaxDaiSupplied);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.balance);
     });
 
     it("when repaying", async () => {
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.repay);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.repay
+      );
 
-      const totalBorrow = adapter.getUserMarketsData()[Underlying.dai]?.totalBorrow!;
-      const walletBalance = adapter.getUserMarketsData()[Underlying.dai]?.walletBalance;
+      const totalBorrow =
+        adapter.getUserMarketsData()[Underlying.dai]?.totalBorrow!;
+      const walletBalance =
+        adapter.getUserMarketsData()[Underlying.dai]?.walletBalance;
 
       // If this expectation is met, the limiter should be the balance:
       expect(walletBalance).toBnGt(totalBorrow);
@@ -290,10 +392,13 @@ describe("getUserMaxCapacity", () => {
         },
       };
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.supply);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.supply
+      );
       expect(maxCapacity?.amount).toBnEq(constants.Zero);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.operationPaused);
     });
@@ -311,8 +416,8 @@ describe("getUserMaxCapacity", () => {
         },
       };
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
       const maxCapacity = adapter.getUserMaxCapacity(
         Underlying.dai,
@@ -335,10 +440,13 @@ describe("getUserMaxCapacity", () => {
         },
       };
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.borrow);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.borrow
+      );
       expect(maxCapacity?.amount).toBnEq(constants.Zero);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.operationPaused);
     });
@@ -356,10 +464,13 @@ describe("getUserMaxCapacity", () => {
         },
       };
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.withdraw);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.withdraw
+      );
       expect(maxCapacity?.amount).toBnEq(constants.Zero);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.operationPaused);
     });
@@ -377,8 +488,8 @@ describe("getUserMaxCapacity", () => {
         },
       };
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
       const maxCapacity = adapter.getUserMaxCapacity(
         Underlying.dai,
@@ -401,10 +512,13 @@ describe("getUserMaxCapacity", () => {
         },
       };
       adapter = MorphoAaveV3Adapter.fromMock(mock);
-      await adapter.refreshAll();
       await adapter.connect(userAddress);
+      await adapter.refreshAll();
 
-      const maxCapacity = adapter.getUserMaxCapacity(Underlying.dai, TransactionType.repay);
+      const maxCapacity = adapter.getUserMaxCapacity(
+        Underlying.dai,
+        TransactionType.repay
+      );
       expect(maxCapacity?.amount).toBnEq(constants.Zero);
       expect(maxCapacity?.limiter).toBe(MaxCapacityLimiter.operationPaused);
     });
