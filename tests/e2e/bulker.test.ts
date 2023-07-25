4639a1858472e63dcd723eca17198187507f9184
Guillaume Hivert <hivert.is.coming@gmail.com>
2023-07-25 17:42:19 +0200

    test(bulker-e2e): Borrow ETH with previous collateral and unwrap
    Signed-off-by: Guillaume Hivert <hivert.is.coming@gmail.com>

--- tests/e2e/bulker.test.ts
+++ tests/e2e/bulker.test.ts
@@ -580,5 +580,57 @@ describe("MorphoAaveV3 Bulker", () => {
         );
       });
     });
+
+    it("should borrow ETH with a previous collateral position and unwrap", async () => {
+      await approve(CONTRACT_ADDRESSES.bulker, async () => {
+        await morphoAaveV3.supplyCollateral(
+          Underlying.dai,
+          initialDaiBalance,
+          morphoUser.address
+        );
+        await morphoAdapter.refreshAll("latest");
+        const oldBalance = await morphoUser.getBalance();
+        const amountToBorrow = utils.parseEther("1");
+        await bulker.addOperations([
+          {
+            type: TransactionType.borrow,
+            amount: amountToBorrow,
+            underlyingAddress: Underlying.weth,
+            unwrap: true,
+          },
+        ]);
+        for (const signature of bulker.signatures$.getValue()) {
+          // @ts-ignore
+          await bulker.sign(signature);
+        }
+        await bulker.executeBatch();
+
+        const ma3Balance = await morphoAaveV3.borrowBalance(
+          weth.address,
+          morphoUser.address
+        );
+        expect(ma3Balance).to.equal(
+          amountToBorrow,
+          "ma3 balance should be the borrowed amount"
+        );
+
+        expect(await weth.balanceOf(addresses.bulker)).to.be.equal(
+          constants.Zero,
+          "weth balance left is not 0"
+        );
+
+        const wethBalance = await weth.balanceOf(morphoUser.address);
+        expect(wethBalance).to.equal(
+          initialWethBalance,
+          "weth balance should not changed"
+        );
+
+        // Expect oldBalance < finalAmount < oldBalance + amount
+        // Trick to avoid gas fees with ETH
+        const finalAmount = await morphoUser.getBalance();
+        expect(oldBalance).to.be.lessThan(finalAmount);
+        expect(finalAmount).to.be.lessThan(oldBalance.add(amountToBorrow));
+      });
+    });
   });
 });
