ecf82b6537f5ccfeb23fdeb683fbc3adf6cf994a
Guillaume Hivert <hivert.is.coming@gmail.com>
2023-07-27 11:23:41 +0200

    refactor(bn): use constant for approx equal threshold
    Signed-off-by: Guillaume Hivert <hivert.is.coming@gmail.com>

--- tests/helpers/bn.ts
+++ tests/helpers/bn.ts
@@ -1,7 +1,9 @@
 import { BigNumber, BigNumberish } from "ethers";
 
+const APPROX_EQUAL_THRESHOLD = 10;
+
 export const approxEqual = (value: BigNumberish, equality: BigNumberish) => {
   const a = BigNumber.from(value);
   const b = BigNumber.from(equality);
-  return a.sub(b).abs().lte(10);
+  return a.sub(b).abs().lte(APPROX_EQUAL_THRESHOLD);
 };
