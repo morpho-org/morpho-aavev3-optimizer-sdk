9016a891dabf3305329bd5295546b5ae69cc150a
Guillaume Hivert <hivert.is.coming@gmail.com>
2023-08-08 16:35:19 +0200

    fix(BN): refactor parameters name

--- tests/helpers/bn.ts
+++ tests/helpers/bn.ts
@@ -2,8 +2,8 @@ import { BigNumber, BigNumberish } from "ethers";
 
 const APPROX_EQUAL_THRESHOLD = 10;
 
-export const approxEqual = (value: BigNumberish, equality: BigNumberish) => {
-  const a = BigNumber.from(value);
-  const b = BigNumber.from(equality);
+export const approxEqual = (a: BigNumberish, b: BigNumberish) => {
+  a = BigNumber.from(a);
+  b = BigNumber.from(b);
   return a.sub(b).abs().lte(APPROX_EQUAL_THRESHOLD);
 };
