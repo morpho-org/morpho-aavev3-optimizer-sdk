2c9b22384e262c1b6e75cdb8afc706a7de076bf5
Oumar <o_fall@orange.fr>
2023-08-15 16:06:22 +0200

    test: update e2e tests

--- tests/helpers/bn.ts
+++ tests/helpers/bn.ts
@@ -2,8 +2,12 @@ import { BigNumber, BigNumberish } from "ethers";
 
 const APPROX_EQUAL_THRESHOLD = 10;
 
-export const approxEqual = (a: BigNumberish, b: BigNumberish) => {
+export const approxEqual = (
+  a: BigNumberish,
+  b: BigNumberish,
+  delta = APPROX_EQUAL_THRESHOLD
+) => {
   a = BigNumber.from(a);
   b = BigNumber.from(b);
-  return a.sub(b).abs().lte(APPROX_EQUAL_THRESHOLD);
+  return a.sub(b).abs().lte(delta);
 };
